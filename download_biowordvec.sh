d868da2db54980420d037b612aa244b56be19e78
stefano-marchesin <34963894+stefano-marchesin@users.noreply.github.com>
2021-07-20 01:54:16 +0200

    Rename download_biowordvec.sh to pretrain/download_biowordvec.sh

--- download_biowordvec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir ./biowordvec
-cd ./biowordvec
-wget --no-check-certificate https://ndownloader.figshare.com/files/12551780 -O biowordvec.bin
---
+++ pretrain/download_biowordvec.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir ./biowordvec
+cd ./biowordvec
+wget --no-check-certificate https://ndownloader.figshare.com/files/12551780 -O biowordvec.bin
