95c262ad5b79c773506d68820612e77f8f153a6e
stefano-marchesin <34963894+stefano-marchesin@users.noreply.github.com>
2021-07-20 09:42:08 +0200

    Update convert_biorel2opennre.sh

--- convert2opennre/convert_biorel2opennre.sh
+++ convert2opennre/convert_biorel2opennre.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cd ../biorel
+cd ../benchmark/biorel
 mv relation2id.json biorel_rel2id.json
 cd ../convert2opennre 
 python convert_biorel2opennre.py
-cd ../biorel
+cd ../benchmark/biorel
 rm train.json
 rm dev.json
 rm test.json
