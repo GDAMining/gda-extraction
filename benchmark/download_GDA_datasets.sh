403680198a1fce44ac613f48f239abfee90d1d52
stefano-marchesin <34963894+stefano-marchesin@users.noreply.github.com>
2022-01-27 18:29:12 +0100

    Update and rename download_GDA_datasets.sh to download_TBGA_dataset.sh

--- benchmark/download_GDA_datasets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://zenodo.org/record/5113853/files/GDA_Datasets.zip
-unzip GDA_Datasets.zip
-rm GDA_Datasets.zip
---
+++ benchmark/download_TBGA_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://zenodo.org/record/5911097/files/TBGA.zip
+unzip TBGA.zip
+rm TBGA.zip
